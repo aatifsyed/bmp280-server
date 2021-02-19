57c5686db129df78ffcba6946917bf4ebd82db15
Aatif Syed <aatif.syed@metaswitch.com>
2021-02-19 13:55:47 +0000

    use warp

--- src/bin/main.rs
+++ src/bin/main.rs
@@ -1,20 +1,12 @@
 use bme280::BME280; // Also BMP280 compatible
-use error_chain::error_chain;
-use hyper::service::{self, Service};
-use hyper::{Body, Request, Response};
 use linux_embedded_hal::{Delay, I2cdev};
-use serde::Serialize;
-use serde_json;
-use std::cell::{RefCell, RefMut};
 use std::net::SocketAddr;
+use std::sync::{Arc, Mutex};
 use structopt::StructOpt;
+use warp::{self, Filter};
 
-error_chain! {}
-
-thread_local!(static SENSOR: RefCell<Option<BME280<I2cdev,Delay>>> = RefCell::new(None));
-
-fn parse_hex(src: &str) -> Result<u8> {
-    Ok(u8::from_str_radix(src, 16).chain_err(|| "foo")?)
+fn parse_hex(src: &str) -> Result<u8, std::num::ParseIntError> {
+    Ok(u8::from_str_radix(src, 16)?)
 }
 
 #[derive(StructOpt)]
@@ -33,32 +25,32 @@ struct Opt {
     socket: SocketAddr,
 }
 
-async fn measurement(_req: Request<Body>) -> Result<Response<Body>> {
-    let ret;
-    SENSOR.with(|tld| {
-        let sensor: Option<BME280<I2cdev, Delay>> = *tld.try_borrow_mut()?;
-        Ok(Response::new("foo".into()))
-    });
-    ret
-}
-
 #[tokio::main]
-async fn _main() -> Result<()> {
+async fn main() -> Result<(), Box<dyn std::error::Error>> {
     let opt = Opt::from_args();
-    let i2c_bus = I2cdev::new(opt.i2c_bus_path).chain_err(|| "Couldn't open i2c device")?;
+    let i2c_bus = I2cdev::new(opt.i2c_bus_path)?;
     let mut sensor = BME280::new(i2c_bus, opt.i2c_address, Delay);
-    sensor.init().chain_err(|| "Couldn't initialize device")?;
+    sensor.init()?;
 
-    SENSOR.with(|tld| *tld.borrow_mut() = Some(sensor));
+    let sensor = Arc::new(Mutex::new(sensor));
+
+    let route = warp::path("measurement")
+        .and(warp::get())
+        // Give a copy of the sensor to each handler thread
+        .and(warp::any().map(move || sensor.clone()))
+        .and_then(handle_measurement);
+
+    warp::serve(route).run(opt.socket).await;
 
     Ok(())
 }
 
-fn main() {
-    if let Err(err) = _main() {
-        for e in err.iter() {
-            eprintln!("{}", e)
-        }
-        std::process::exit(1);
+async fn handle_measurement(
+    sensor: Arc<Mutex<BME280<I2cdev, Delay>>>,
+) -> Result<Box<dyn warp::Reply>, warp::Rejection> {
+    let mut sensor = sensor.lock().unwrap();
+    match sensor.measure() {
+        Ok(measurement) => Ok(Box::new(format!("{:#?}", measurement))),
+        Err(err) => Ok(Box::new(format!("{:#?}", err))),
     }
 }
